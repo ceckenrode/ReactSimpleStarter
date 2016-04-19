22f096cad11d38d6579a875cafe0aeec0c4a9f39
Christian Eckenrode Work <christianeckenrodework@Christians-MacBook-Pro.local>
2016-04-19 02:10:37 -0400

    added mroe components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, {
+  Component
+} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search'
+import SearchBar from './components/search_bar';
+import VideoList from './components//video_list';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: []
+    };
+    const API_KEY = 'AIzaSyD6h710E57zBn7e8oBEC8yrC4yLGugA8Sw';
+    YTSearch({
+      key: API_KEY,
+      term: 'surfboards'
+    }, (videos) => {
+      this.setState({videos})
+      console.log(videos);
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar/>
+        <VideoList videos="{this.state.videos}"/>
+      </div>
+    );
+  };
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render( <App/> , document.querySelector('.container'));
