fb4280f9eba0b9be9d8f4689f2e19af13a3fae7b
Christian Eckenrode Work <christianeckenrodework@Christians-MacBook-Pro.local>
2016-04-22 01:41:47 -0400

    rendered youtube videos to page

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail.js';
 
 
 class App extends Component {
@@ -27,6 +28,7 @@ class App extends Component {
   render() {
     return ( <div>
       <SearchBar/>
+      <VideoDetail video={this.state.videos[0]}/>
       <VideoList videos={this.state.videos}/>
       </div>
     );
